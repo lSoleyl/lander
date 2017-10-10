9596a2c14fc1aac9174a2569aa113d5d0752e965
lSoleyl <lSoleyl+github@gmail.com>
2017-10-10 18:58:59 +0200

    Added vector constants and division operator

--- Lander/Vector.cpp
+++ Lander/Vector.cpp
@@ -1,5 +1,12 @@
 #include "stdafx.h"
 
+
+const Vector Vector::Zero(0,0);
+const Vector Vector::Up(0,-1); //Viewport's vertical axis is reversed
+const Vector Vector::Down(0,1);
+const Vector Vector::Left(-1,0);
+const Vector Vector::Right(1,0);
+
 Vector::Vector(float x, float y) : x(x), y(y) {}
 Vector::Vector(const Vector& other) : x(other.x), y(other.y) {}
 Vector::Vector(Vector&& other) : x(other.x), y(other.y) {}
@@ -23,13 +30,19 @@ Vector& Vector::operator+=(const Vector& other) {
 }
 
 Vector& Vector::operator-=(const Vector& other) {
-  return (*this += other * -1);
+  x -= other.x;
+  y -= other.y;
+  return *this;
 }
 
 Vector& Vector::operator*=(float factor) {
   x *= factor;
   y *= factor;
   return *this;
+}
+
+Vector& Vector::operator/=(float divisor) {
+  return *this *= (1 / divisor);
 }
 
 Vector Vector::operator+(const Vector& other) const {
@@ -42,6 +55,10 @@ Vector Vector::operator-(const Vector& other) const {
   
 Vector Vector::operator*(float factor) const {
   return Vector(x*factor, y*factor);
+}
+
+Vector Vector::operator/(float divisor) const {
+  return *this * (1/divisor);
 }
 
 float Vector::Length() const {
--- Lander/Vector.hpp
+++ Lander/Vector.hpp
@@ -16,16 +16,25 @@ public:
   Vector& operator+=(const Vector& other);
   Vector& operator-=(const Vector& other);
   Vector& operator*=(float factor);
+  Vector& operator/=(float divisor);
 
   Vector operator+(const Vector& other) const;
   Vector operator-(const Vector& other) const;
   Vector operator*(float factor) const;
+  Vector operator/(float divisor) const;
 
 
   //Vector length
   float Length() const;
 
   float x,y;
+
+  //Constant vectors
+  static const Vector Zero;
+  static const Vector Up;
+  static const Vector Down;
+  static const Vector Left;
+  static const Vector Right;
 };
 
 Vector operator*(float factor, const Vector& v);
