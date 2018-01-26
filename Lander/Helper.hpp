98ed2b978e0c4356b7b447821aa1344f185fb3db
lSoleyl <lsoleyl+github@gmail.com>
2018-01-26 19:18:05 +0100

    Added OutputDebugStream-Makro to helper-header

--- Lander/Helper.hpp
+++ Lander/Helper.hpp
@@ -1,7 +1,15 @@
 #pragma once
-#include "stdafx.h"
 
-bool KeyPressed(int key) {
+#include <sstream>
+
+inline bool KeyPressed(int key) {
   // Don't know why, but GetAsyncKeyState() doesn't work correctly when called to often
   return (GetKeyState(key) & (1 << 7)) != 0;
 }
+
+
+#define OutputDebugStream(text) do { \
+    std::ostringstream out; \
+    out << text << std::endl; \
+    OutputDebugString(out.str().c_str()); \
+  } while(0)
